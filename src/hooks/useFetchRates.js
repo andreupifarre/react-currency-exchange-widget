1c8e28396a589c507a2a76ae24fcb858cf8ccb76
Andreu Pifarre <andreupifarre@gmail.com>
2020-10-17 21:23:31 +0100

    added extra pngs

--- src/hooks/useFetchRates.js
+++ src/hooks/useFetchRates.js
@@ -3,8 +3,6 @@ import { useState, useEffect } from 'react';
 const useFetchRates = (fromCurrency, interval) => {
   const [rates, setRates] = useState(null);
 
-  if (!fromCurrency) return null;
-
   useEffect(() => {
     const fetchData = async () => {
       const url = `https://openexchangerates.org/api/latest.json?app_id=81fa12e332b74f45a1de4a276db7c0d1&base=${fromCurrency}`;
